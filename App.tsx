e4f2d49e79bd000f26b63bd33d8d6fc861319430
Danylo Oliveira <danylo.oliveira73@gmail.com>
2022-12-28 17:12:06 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {Text, StatusBar} from 'react-native';
+import {NativeBaseProvider} from 'native-base';
+// import {
+//   useFonts,
+//   Roboto_400Regular,
+//   Roboto_700Bold,
+// } from '@expo-google-fonts/roboto';
+import React from 'react';
+import {Loading} from '@components/Loading';
+import {THEME} from './src/theme';
+import {SignUp} from '@screens/SignUp';
+export default function App() {
+  // const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <SignUp />
+    </NativeBaseProvider>
+  );
+}
