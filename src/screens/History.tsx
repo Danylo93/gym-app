05c58b500ff629372ae2817230937ff7555c2e73
Danylo Oliveira <danylo.oliveira73@gmail.com>
2022-12-30 16:06:58 -0300

    feature: Profile

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,50 @@
-import { Center, Text } from 'native-base';
-import React from 'react';
+import { HistoryCard } from '@components/HistoryCard';
+import { ScreenHeader } from '@components/ScreenHeader';
+
+import { Center, Heading, Text, VStack, SectionList } from 'native-base';
+import React, { useState } from 'react';
 
 export function History(){
+
+const  [ exercises, setExercises] = useState([
+    {
+        title : '26.12.2022',
+        data: ['Puxada Frontal', 'Remada Frontal']
+    },
+    {
+        title : '28.12.2022',
+        data: ['Biceps', 'Triceps']
+    },
+
+
+])
+
     return(
-        <Center flex={1}>
-            <Text color='white'>History</Text>
-        </Center>
+        <VStack flex={1}>
+           <ScreenHeader title='Histórico de Exercícios'/>
+
+           <SectionList
+           sections={exercises}
+           keyExtractor={item => item}
+           renderItem={({ item}) => (
+            <HistoryCard />
+           )}
+           renderSectionHeader={({section}) => (
+            <Heading color='gray.200' fontSize='md' mt={10} mb={3}>
+                {section.title}
+            </Heading>
+           )}
+           px={8}
+           contentContainerStyle={[].length === 0  && {flex: 1, justifyContent: 'center'}}
+           ListEmptyComponent={() => (
+            <Text color='gray.100' textAlign='center'>
+                Não há exercícios registrados ainda. {'\n'}
+                Vamos nos exercitar hoje?
+            </Text>
+           )}
+           />
+          
+        </VStack>
+        
     )
 }
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,72 @@
-import { Center, Text } from 'native-base';
-import React from 'react';
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
+import { TouchableOpacity } from 'react-native'
+import { Center, ScrollView, Text, VStack, Skeleton, Heading } from 'native-base';
+import React, { useState } from 'react';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+
+const PHOTO_SIZE = 33;
 
 export function Profile(){
+
+    const  [photoLoading, setPhotoLoading] = useState(false);
     return(
-        <Center flex={1}>
-            <Text color='white'>Profile</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title='Perfil' />
+            <ScrollView contentContainerStyle={{ paddingBottom: 36}}>
+                <Center mt={6} px={10}>
+                    {
+                    photoLoading ?
+                    <Skeleton 
+                    w={PHOTO_SIZE} 
+                    h={PHOTO_SIZE} 
+                    rounded='full'
+                    startColor='gray.500'
+                    endColor='gray.400'
+                    /> :
+                    <UserPhoto 
+                    source={{uri: 'https://github.com/Danylo93.png'}}
+                    size={PHOTO_SIZE}
+                    alt="Imagem user"
+                    mr={4}
+                    />
+                    }
+
+                    <TouchableOpacity>
+                        <Text color='green.500' fontWeight='bold' fontSize='md' mt={2} mb={8}> Alterar Foto</Text>
+                    </TouchableOpacity>
+
+                    <Input bg='gray.600' placeholder='Nome' />
+                    <Input bg='gray.600' placeholder='Email' isDisabled  />
+                </Center>
+
+                <Center px={10} mt={12} mb={9}>
+                    <Heading color='gray.200' fontSize='md' mb={2} alignSelf='flex-start' mt={8}>
+                        Alterar Senha
+                    </Heading>
+
+                    <Input
+                    bg='gray.600'
+                    placeholder='Senha Antiga'
+                    secureTextEntry
+                    />
+                    <Input
+                    bg='gray.600'
+                    placeholder='Nova Senha'
+                    secureTextEntry
+                    />
+                    <Input
+                    bg='gray.600'
+                    placeholder='Confirme a nova senha'
+                    secureTextEntry
+                    />
+                    <Button title='Atualizar' mt={4} />
+                </Center>
+                
+            </ScrollView>
+            
+           
+        </VStack>
     )
 }
